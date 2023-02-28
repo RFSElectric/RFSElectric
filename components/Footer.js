84eb194ee4065b45598b78d8ef80ada235bcfb03
RFSElectric <126535868+RFSElectric@users.noreply.github.com>
2023-02-27 18:25:33 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,12 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
+    <footer className={styles.footer}>
+        <ul class="mylinks">
+             <li><a href="https://www.yelp.com/biz/rfs-electric-tarzana-2" target="_blank">Check us on Yelp</a></li>
+             <li><a href="https://fargosoftware.netlify.app/" target="_blank">Made by Fargo Software</a></li>
+        </ul>      
+    </footer>
     </>
   )
 }
